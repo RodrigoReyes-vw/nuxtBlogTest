78a76267928d448e19a4759d0b13d3b0ddc7573f
Rodrigo Reyes <rsanchez@vectorworks.net>
2024-05-08 11:49:25 -0400

    removed messy stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,4 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@formkit/auto-animate", "@nuxt/content", "@vueuse/nuxt"],
   extends: "../snowDog/base/",
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-      routes: ["/blog/**"],
-      ignore: ["/**"],
-      failOnError: true,
-    },
-  },
 });
